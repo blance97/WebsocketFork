26bebf23fee4aa7bc4f40a7d9bbb05f369990207
blance97 <lancedinh7@gmail.com>
2016-08-05 02:28:35 -0400

    prob need to add angular back in

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
+
 type User struct {
-	Username	string
+	Username string
 }
+
 //TODO Use the Sync.Mutex in every function where you use the map concurrently.
 var connections map[*websocket.Conn]bool
+var Users = User{}
+
+func main() {
+	// command line flags
+	port := flag.Int("port", 80, "port to serve on")
+	dir := flag.String("directory", "web/", "directory of web files")
+	flag.Parse()
+
+	connections = make(map[*websocket.Conn]bool)
+
+	// handle all requests by serving a file of the same name
+	fs := http.Dir(*dir)
+	fileHandler := http.FileServer(fs)
+	http.Handle("/", fileHandler)
+	http.HandleFunc("/ws", wsHandler)
+	http.HandleFunc("/getUser", getUsername)
+	http.HandleFunc("/storeUser", storeUsername)
+	log.Printf("Running on port %d\n", *port)
+
+	addr := fmt.Sprintf("127.0.0.1:%d", *port)
+	// this call blocks -- the progam runs here forever
+	err := http.ListenAndServe(addr, nil)
+	fmt.Println(err.Error())
+}
 
 func getJSON(r *http.Request) map[string]interface{} {
 	var data map[string]interface{}
 
-	log.Printf("getJSON:\tBegin execution")
+	//	log.Printf("getJSON:\tBegin execution")
 	if r.Body == nil {
 		log.Printf("No Request Body")
 	}
@@ -28,20 +55,28 @@ func getJSON(r *http.Request) map[string]interface{} {
 	return data
 } //decode JSON
 
-
-func storeUsername(w http.ResponseWriter, r *http.Request){
-	log.Printf("Store User Handler")
-	defer log.Printf("done Store User Handler")
-	data:= getJSON(r)
-	//username = User{Username: data["Username"].(string)} //store in database later
+//Anything we write to w will be send to the client.  and r represents the request.
+func storeUsername(w http.ResponseWriter, r *http.Request) {
+	//log.Printf("Store User Handler")
+	//defer log.Printf("done Store User Handler")
+	data := getJSON(r)
 	Users.Username = data["Username"].(string)
 }
-func getUsername(w http.ResponseWriter, r *http.Request){
-	log.Prinf("Get User Handler")
-	defer log.Printf("done Get User Handler")
+func getUsername(w http.ResponseWriter, r *http.Request) {
+	//	log.Printf("Get User Handler")
+	//defer log.Printf("done Get User Handler")
 	json.NewEncoder(w).Encode(Users)
 }
+func sendAll(msg []byte) {
+	for conn := range connections {
+		if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			delete(connections, conn)
+			return
+		}
+	}
+}
 func wsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("called")
 	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
 	if _, ok := err.(websocket.HandshakeError); ok {
 		http.Error(w, "Not a websocket handshake", 400)
@@ -52,34 +87,17 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 	connections[conn] = true
-	for {//inf loop
+	for { //inf loop
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			delete(connections, conn)
 			return
 		}
-
+			if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+			fmt.Println("sent")//does this even do anything?
+			return
+		}
 		log.Println(string(msg))
+		sendAll(msg)
 	}
-}
-func main() {
-	// command line flags
-	port := flag.Int("port", 80, "port to serve on")
-	dir := flag.String("directory", "web/", "directory of web files")
-	flag.Parse()
-	var Users = []User
-	connections = make(map[*websocket.Conn]bool)
-
-	// handle all requests by serving a file of the same name
-	fs := http.Dir(*dir)
-	fileHandler := http.FileServer(fs)
-	http.Handle("/", fileHandler)
-	http.HandleFunc("/ws", wsHandler)
-
-	log.Printf("Running on port %d\n", *port)
-
-	addr := fmt.Sprintf("127.0.0.1:%d", *port)
-	// this call blocks -- the progam runs here forever
-	err := http.ListenAndServe(addr, nil)
-	fmt.Println(err.Error())
 }
--- web/js/chat.js
+++ web/js/chat.js
@@ -1,29 +1,56 @@
-var app = angular.module("chat", []);
+    var ws = new WebSocket("ws://localhost/ws");
+    var fromMe = false;
+    ws.onopen = function() {
+        $("#ChatPanel").html("CONNECTED")
+    };
+    ws.onclose = function() {
+        $("#ChatPanel").html("DISCONNECTED")
+    };
+    ws.onmessage = function(event) {
 
-app.controller("MainCt1", ["$scope", function($scope) {
-    $scope.messages = []; // list that we recive from server
-
-    var conn = new WebSocket("ws://localhost/ws");
-
-    conn.onclose = function(event) {
-        $scope.$apply(function() {
-            $scope.messages.push("DISCONNECTED")
-        })
-    }
-    conn.onopen = function(event) {
-        $scope.$apply(function() {
-            $scope.messages.push("CONNECTED")
-        })
+      if(!fromMe){
+        $('<div class="clearfix"><blockquote class="you pull-right">' +  event.data + '!</blockquote></div>"').appendTo('#chatbox');
+      }
     }
 
-    conn.onmessage = function(event) {
-        $scope.$apply(function() {
-            $scope.messages.push(event.data);
-        })
-    }
-    $scope.send = function(){
-      conn.send($scope.msg);
-      $scope.msg = "";
-    }
+    $(document).ready(function() {
+        var $Username = $('#inputUsername');
+        $('#inputChat').val("");
+    // take what's the textbox and send it off
+    $('#sendMsgBtn').click( function(event) {
+      fromMe = true;
+      ws.send($('#inputChat').val());
+      $('#inputChat').val("");
+    });
+    fromMe = false;
 
-}])
+      /*  $('#sendMsgBtn').click(function() {
+            $.ajax({
+                type: 'GET',
+                url: '/getUser',
+                success: function(data) {
+                    console.log("Success", data);
+                    value = $("#inputChat").val();
+                    ws.send($Username.val() + ": " + value)
+                    value = ""
+                    $('<div class="clearfix"><blockquote class="me pull-left">' + $Username.val() + ": " + value + '</blockquote></div>"').appendTo('#chatbox');
+
+                }
+            });
+        });*/
+
+        $('#SetNameBtn').click(function() {
+            $.ajax({
+                type: 'POST',
+                url: '/storeUser',
+                data: JSON.stringify({
+                    Username: $Username.val()
+                }),
+                dataType: 'json',
+                success: function(data) {
+                    console.log("Posted Data");
+                }
+            });
+        });
+
+    });
